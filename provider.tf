0b7d4e9a9eb257cab19b36d161d013675755ef45
srivijay1207 <116360935+srivijay1207@users.noreply.github.com>
2023-10-11 18:39:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,8 +2,14 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.20.1"
+      version = "5.15.0"
     }
+  }
+   backend "s3" {
+    bucket   = "rs-remote-state-dev"
+    key = "jenkinsdemo1"
+    region = "us-east-1"
+    dynamodb_table = "dynamodb-table-dev"
   }
 }
 
